3cb382e13c25bf4dea0578891685c7c5ebc1beab
Biki280 <bikig8456@gmail.com>
2024-12-21 22:38:10 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY EVIL4REAL
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7454983271:AAENg67TMVuIuDDA1BqCb_g7Gw23HJe6uyA)
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Sarkar3009")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7972657976")
+
+
+
+
+
+
+
+  
